54b68562bfbd1de93b1e5b4c61b086a8641dea2c
BugsDev2 <msherlock246@gmail.com>
2022-10-24 22:25:51 +0530

    Added the two files -- html and javascript files of the first draft

---
+++ js.js
@@ -0,0 +1,17 @@
+
+const btn = document.querySelector('#btn');
+const root = document.querySelector(':root');
+
+function random(num){
+	return Math.floor(Math.random() * num);
+}
+
+
+
+//~ console.log(random(256), random(256), random(256))
+btn.addEventListener('click', function(){
+	root.style.setProperty('--r', random(256));
+	root.style.setProperty('--g', random(256));
+	root.style.setProperty('--b', random(256));
+	document.body.style = 'background: rgba(var(--r),var(--g),var(--b))'
+})
